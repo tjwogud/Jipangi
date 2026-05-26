3620e120ce69e81487bffb14b19cbd871721e63e
tjwogud <rlatjsal78@gmail.com>
2026-05-26 23:05:37 +0900

    wowowowowow

--- app/page.js
+++ app/page.js
@@ -1,9 +1,43 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 import Image from "next/image";
 import styles from "./page.module.css";
 import logo from "./assets/images/jipangi.png"
 import digital_bg from "./assets/images/digital_bg.png"
 
+function Item({id, data}) {
+  return (
+    <a className={styles.item}
+      href={`./apps/${id}/instruction.pdf`}
+      target="_blank"
+      rel="noopener noreferrer">
+      <img
+        alt={data.title}
+        src={`./apps/${id}/icon.png`}/>
+      <p>{data.title}</p>
+    </a>
+  )
+}
+
 export default function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("./apps/apps.txt")
+      .then(res => res.text())
+      .then(txt =>
+        Promise.all(
+          txt.split('\n')
+          .map(v => v.trim())
+          .filter(Boolean)
+          .map(id => fetch(`./apps/${id}/data.json`)
+            .then(res => res.json())
+            .then(data => ({id, data})))))
+      .then(setData);
+  }, []);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
@@ -16,7 +50,8 @@ export default function Home() {
       <Image
         alt="디지털 배경"
         src={digital_bg}
-        className={styles.digital_bg}/>
+        className={styles.digital_bg}
+        priority/>
       <p className={styles.overlay_1}>
         앱 사용이 어려운 당신을 위한,<br/>
         어떤 앱이 필요한지 모르는 당신을 위한,
@@ -25,18 +60,13 @@ export default function Home() {
       <p className={styles.overlay_3}>by 반박E와 I들</p>
       <p className={styles.scroll_down}>↓↓↓ 아래로 내려가 시작하세요</p>
       <div className={styles.grid_container}>
-        <div className={styles.item}>
-          <img 
-            alt="네이버 지도"
-            src="./apps/naver_map/icon.png"/>
-          <p>네이버 지도</p>
-        </div>
-        <div className={styles.item}>
-          <img 
-            alt="앱 이름"
-            src="./apps/kakao_taxi/icon.png"/>
-          <p>카카오 택시</p>
-        </div>
+        {data.map(v => {
+          <Item
+            key={v.id}
+            id={v.id}
+            data={v.data}
+          />
+        })}
       </div>
     </div>
   );
