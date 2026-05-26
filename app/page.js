9305615ed35bcf4114ed68b4c1865f48a186cc7a
tjwogud <rlatjsal78@gmail.com>
2026-05-26 22:06:38 +0900

    fantastic commit

--- app/page.js
+++ app/page.js
@@ -1,66 +1,43 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import logo from "./assets/images/jipangi.png"
+import digital_bg from "./assets/images/digital_bg.png"
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <div className={styles.container}>
+      <h1 className={styles.title}>
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+          alt="지팡이 로고"
+          src={logo}
+          className={styles.icon}/>
+        &#32;지팡이<br/>
+        <p className={styles.subtitle}>: 모두를 위한 앱 설명서</p></h1>
+      <Image
+        alt="디지털 배경"
+        src={digital_bg}
+        className={styles.digital_bg}/>
+      <p className={styles.overlay_1}>
+        앱 사용이 어려운 당신을 위한,<br/>
+        어떤 앱이 필요한지 모르는 당신을 위한,
+      </p>
+      <p className={styles.overlay_2}>앱 설명서, 지팡이</p>
+      <p className={styles.overlay_3}>by 반박E와 I들</p>
+      <p className={styles.scroll_down}>↓↓↓ 아래로 내려가 시작하세요</p>
+      <div className={styles.grid_container}>
+        <div className={styles.item}>
+          <img 
+            alt="네이버 지도"
+            src="./apps/naver_map/icon.png"/>
+          <p>네이버 지도</p>
         </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className={styles.item}>
+          <img 
+            alt="앱 이름"
+            src="./apps/kakao_taxi/icon.png"/>
+          <p>카카오 택시</p>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
